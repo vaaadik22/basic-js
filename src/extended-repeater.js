1f967a06a0e60357e40e9be0ab6e35458c0726c0
vaaadik22 <vaaadik22@gmail.com>
2022-10-15 12:40:53 +0300

    feat:add solution exended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,8 +16,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const repeatTimes = options.repeatTimes || 1;
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const separator = options.separator || '+';
+  const additionSeparator = options.additionSeparator || '|';
+  let addition = '';
+  if (options.hasOwnProperty('addition')) addition = String(options.addition);
+  
+  let arrStr = [], arrAdd = []
+  for (let j = 0; j < additionRepeatTimes; j++) {
+    arrAdd.push(String(addition));
+    
+  }
+  let strokaAdd = arrAdd.join(additionSeparator);
+  for (let i = 0; i < repeatTimes; i++) {
+     arrStr.push(String(str) + strokaAdd);
+  }
+  return arrStr.join(separator);
 }
 
 module.exports = {
