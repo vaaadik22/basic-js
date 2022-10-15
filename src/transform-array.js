01a878207fbcbce7ee5a0e7bd0aaa85b6927c651
vaaadik22 <vaaadik22@gmail.com>
2022-10-15 13:11:22 +0300

    feat:add soltion transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,8 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) throw new Error (`'arr' parameter must be an instance of the Array!`);
+  let modifyArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next' :
+        i += 2;
+        continue;
+      case '--discard-prev' :
+        if (modifyArray.length > 1) modifyArray.pop();
+        continue;
+      case '--double-next' :
+        if (arr.length > i + 1) modifyArray.push(arr[i + 1]); 
+        continue;
+      case '--double-prev' :
+        if (modifyArray.length > 1) modifyArray.push(modifyArray[modifyArray.length - 1]);
+        continue;
+    }
+    modifyArray.push(arr[i]);
+  };
+  return modifyArray;
 }
 
 module.exports = {
