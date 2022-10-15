bf499565443e9cccb8cd85c03f0adb9c066fbc50
vaaadik22 <vaaadik22@gmail.com>
2022-10-15 12:38:32 +0300

    feat:add solution encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,8 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let str = '';
+  let subStr = '';
+  str.split('').forEach((el, i) => {
+    if (subStr == '') {
+      subStr = el;
+    } else if (el == subStr[0]) {
+      subStr += el;
+    } else {
+      if (subStr.length == 1) {
+        str += subStr;
+      } else {
+        str += subStr.length + subStr[0];
+      }
+      subStr = el;
+    }
+    if (i == str.length - 1) {
+      if (subStr.length == 1) {
+        str += subStr;
+      } else {
+        str += subStr.length + subStr[0];
+      }
+    }
+  });
+  return str;
 }
 
 module.exports = {
