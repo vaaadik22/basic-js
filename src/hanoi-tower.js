ecdcdcc9bce9d93aec63a6ece7bb719f8a65f8b0
vaaadik22 <vaaadik22@gmail.com>
2022-10-17 23:58:34 +0300

    fix:commented out the function parameter

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
+function calculateHanoi(disksNumber, turnsSpeed) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   const turns = Math.pow(2, disksNumber) - 1;
