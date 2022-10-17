ec5a9efb0accd0e560d331d8d52b43c14352f2b4
vaaadik22 <vaaadik22@gmail.com>
2022-10-17 23:55:30 +0300

    fix:commented out the function parameter

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(members) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   if (!Array.isArray(members)) return false;
