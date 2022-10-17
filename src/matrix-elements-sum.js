dde048496b509185ccdb21b39f47e213c7970763
vaaadik22 <vaaadik22@gmail.com>
2022-10-17 23:59:31 +0300

    fix:commented out the function parameter

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,7 +16,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
+function getMatrixElementsSum(matrix) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   return matrix.reduce((sum, row, i) => {
